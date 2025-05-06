ca122bf636a9486ac3fd7c8e35904069cd8132eb
hOst1k <gubanoff1337@gmail.com>
2025-05-06 13:40:15 +0300

    Try to add translation

--- FilmCatalog/main.cpp
+++ FilmCatalog/main.cpp
@@ -1,17 +1,23 @@
 #include "include/Authorization.h"
 #include "include/FilmCatalog.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
+#include <QLocale>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator translator;
+    QString translationFile = QCoreApplication::applicationDirPath() + "/translations/Catalog_" + QLocale::system().name();
+    translator.load(translationFile);
+    a.installTranslator(&translator);
+    a.setStyle("fusion");
     a.setWindowIcon(QIcon(":/icon/icon.png"));
 #ifdef AUTHORIZATION
     Authorization w;
 #else
     FilmCatalog w;
 #endif // AUTHORIZATION    
-    a.setStyle("fusion");
     w.show();
     return a.exec();
 }
---
+++ FilmCatalog/translation/Catalog_ru.ts
@@ -0,0 +1,376 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>AdminMenu</name>
+    <message>
+        <location filename="../AdminMenu.cpp" line="39"/>
+        <location filename="../AdminMenu.cpp" line="60"/>
+        <source>Not allowed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="39"/>
+        <location filename="../AdminMenu.cpp" line="60"/>
+        <source>You can not edit your self!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="45"/>
+        <source>Failed to delete a user!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="78"/>
+        <source>Wrong password format!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="78"/>
+        <source>You enter a empty password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="82"/>
+        <source>Failed to change password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AdminMenu.cpp" line="82"/>
+        <source>Contact developer!
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AdminMenuClass</name>
+    <message>
+        <location filename="../ui/AdminMenu.ui" line="14"/>
+        <source>Admin Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/AdminMenu.ui" line="25"/>
+        <source>Hello, admin!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Authorization</name>
+    <message>
+        <location filename="../Authorization.cpp" line="28"/>
+        <source>Failed to login!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="29"/>
+        <source>Account created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="29"/>
+        <source>Congratulations! The account was successfully created.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="31"/>
+        <source>Failed to register a user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="31"/>
+        <source>A user with this login already exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="39"/>
+        <source>Login Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="39"/>
+        <source>It appears that no such user exists.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="55"/>
+        <source>Error!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Authorization.cpp" line="55"/>
+        <source>Wrong Password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AuthorizationClass</name>
+    <message>
+        <location filename="../ui/Authorization.ui" line="14"/>
+        <source>Authorisation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Authorization.ui" line="20"/>
+        <location filename="../ui/Authorization.ui" line="27"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Authorization.ui" line="34"/>
+        <location filename="../ui/Authorization.ui" line="44"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Authorization.ui" line="53"/>
+        <source>Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Authorization.ui" line="60"/>
+        <source>Sign In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Authorization.ui" line="74"/>
+        <source>Film Catalog</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatalogDisplayItem</name>
+    <message>
+        <location filename="../ui/CatalogDisplayItem.ui" line="14"/>
+        <source>CatalogDisplayItem</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogDisplayItem.ui" line="102"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatalogItemAdder</name>
+    <message>
+        <location filename="../CatalogItemAdder.cpp" line="10"/>
+        <source>Select film poster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CatalogItemAdder.cpp" line="28"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../CatalogItemAdder.cpp" line="28"/>
+        <source>Empty field detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CatalogItemAdderClass</name>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="14"/>
+        <source>CatalogItemAdder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="54"/>
+        <source>Release:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="84"/>
+        <source>Duration:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="108"/>
+        <source>Director:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="122"/>
+        <source>Actors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="136"/>
+        <source>Studio:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="150"/>
+        <source>Genres:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="166"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="180"/>
+        <source>Import Poster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/CatalogItemAdder.ui" line="187"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayCatalog</name>
+    <message>
+        <location filename="../DisplayCatalog.cpp" line="68"/>
+        <source>Select film poster</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayCatalogClass</name>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="14"/>
+        <source>DisplayCatalog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="58"/>
+        <source>Release:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="79"/>
+        <source>Duration:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="100"/>
+        <source>Director:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="118"/>
+        <source>Actors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="136"/>
+        <source>Studio:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="154"/>
+        <source>Genres:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="174"/>
+        <source>Description:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="188"/>
+        <source>Accept changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/DisplayCatalog.ui" line="195"/>
+        <source>Upload Poster</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FilmCatalog</name>
+    <message>
+        <location filename="../FilmCatalog.cpp" line="98"/>
+        <location filename="../FilmCatalog.cpp" line="99"/>
+        <location filename="../FilmCatalog.cpp" line="203"/>
+        <location filename="../FilmCatalog.cpp" line="262"/>
+        <location filename="../FilmCatalog.cpp" line="263"/>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FilmCatalogClass</name>
+    <message>
+        <location filename="../ui/FilmCatalog.ui" line="14"/>
+        <source>FilmCatalog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/FilmCatalog.ui" line="81"/>
+        <source>Enter filter condition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/FilmCatalog.ui" line="161"/>
+        <source>Add New Film</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserCardClass</name>
+    <message>
+        <location filename="../ui/UserCard.ui" line="14"/>
+        <source>User Card (Embedded)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/UserCard.ui" line="25"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserSelfEdit</name>
+    <message>
+        <location filename="../UserSelfEdit.cpp" line="31"/>
+        <source>Wrong password format!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UserSelfEdit.cpp" line="31"/>
+        <source>You enter a empty password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UserSelfEdit.cpp" line="35"/>
+        <source>Failed to change password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UserSelfEdit.cpp" line="35"/>
+        <source>Contact developer!
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserSelfEditClass</name>
+    <message>
+        <location filename="../ui/UserSelfEdit.ui" line="14"/>
+        <source>UserSelfEdit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/UserSelfEdit.ui" line="25"/>
+        <source>SomeGuy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/UserSelfEdit.ui" line="35"/>
+        <source>Change password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/UserSelfEdit.ui" line="42"/>
+        <source>Log Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
